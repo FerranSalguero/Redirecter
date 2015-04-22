f4032157dba54d3637fa616b82e2cc0eb2150177
Ferran Salguero Mateu <fsalgueromateu@gmail.com>
2015-04-22 20:10:44 +0200

    Redirect to compare

--- Redirecter/Controllers/HomeController.cs
+++ Redirecter/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace Redirecter.Controllers
 {
     public class HomeController : Controller
     {
-        const string baseUrl = "http://wheretobuy.apphb.com";
+        const string baseUrl = "http://compare.apphb.com";
 
         public ActionResult Index(string q = "")
         {
