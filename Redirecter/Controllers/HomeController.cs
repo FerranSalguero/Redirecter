2b5b5a2fed273ef7b39c8d61b8a1ebbd8479be12
Ferran Salguero Mateu <fsalgueromateu@gmail.com>
2016-04-25 10:19:20 +0200

    redirect new route

--- Redirecter/Controllers/HomeController.cs
+++ Redirecter/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace Redirecter.Controllers
     public class HomeController : Controller
     {
 
-        const string baseUrl = "http://wheretobuy.apphb.com";
+        const string baseUrl = "http://wheretobuy.putinyourbasket.com";
 
         public ActionResult Index(string q = "")
         {
