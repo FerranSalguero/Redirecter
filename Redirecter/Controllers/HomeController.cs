d2eb1c261489ad94205ffcd6b5f329f17051b7b2
Ferran Salguero Mateu <fsalgueromateu@gmail.com>
2019-03-01 21:29:30 +0100

    Redirect to whereshould I buy

--- Redirecter/Controllers/HomeController.cs
+++ Redirecter/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace Redirecter.Controllers
     public class HomeController : Controller
     {
 
-        const string baseUrl = "http://wheretobuy.putinyourbasket.com";
+        const string baseUrl = "https://whereshouldIbuy.apphb.com";
 
         [OutputCache(Duration = 3600 * 48)]
         public ActionResult Index(string q = "", string country = "us")
